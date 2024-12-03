533afe45487c8d19ea3b1e5c02cf4e5d9e0bcd5e
peris.molina.adria@alumnat.copernic.cat <peris.molina.adria@alumnat.copernic.cat>
2024-12-03 12:48:43 +0100

    Proyecto inicial

---
+++ src/test/java/Tennis_ERP/TennisErp/TennisErpApplicationTests.java
@@ -0,0 +1,13 @@
+package Tennis_ERP.TennisErp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
