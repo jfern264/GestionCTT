01609007775671ee68019bdc5eddd2b61ab427df
peris.molina.adria@alumnat.copernic.cat <peris.molina.adria@alumnat.copernic.cat>
2025-01-13 12:47:33 +0100

    base menu administrador

--- src/main/java/Tennis_ERP/TennisErp/Controller/MenuPrincipalController.java
+++ src/main/java/Tennis_ERP/TennisErp/Controller/MenuPrincipalController.java
@@ -1,21 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/SpringFramework/Controller.java to edit this template
- */
 package Tennis_ERP.TennisErp.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- *
- * @author Adria
- */
+
 @Controller
 public class MenuPrincipalController {
 
     @GetMapping("/menu_principal")
     public String menu_principal() {
         return "Menu_principal";  // Retorna el nombre de la vista (Menu_principal.html)
+    }
+    
+    @GetMapping("/menu_admin")
+    public String menu_admin() {
+        return "menu_admin";  // Retorna el nombre de la vista (menu_admin.html)
     }
 }
