91117968b120fe0d6d7a40d03cbcbb605c1a4586
danie <danieladanegbe@alumnat.copernic.cat>
2025-12-19 18:26:57 +0100

    Actualizacion temporal y uso de DB en la nube

--- src/main/java/Tennis_ERP/TennisErp/DAO/RolDAO.java
+++
@@ -1,9 +0,0 @@
-package Tennis_ERP.TennisErp.DAO;
-
-import Tennis_ERP.TennisErp.Domain.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolDAO extends JpaRepository<Rol, Long> {
-
-    Rol findByNombreRol(String nombreRol);
-}
